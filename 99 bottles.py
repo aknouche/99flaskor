cfe4631106cbca0f6885e6e0e838e3823496858f
aknouche <42774809+aknouche@users.noreply.github.com>
2019-07-02 11:08:11 +0200

    Add files via upload

---
+++ 99 bottles.py
@@ -0,0 +1,15 @@
+i = 100
+while i > 0:
+    i -= 1
+    if i > 1:
+        print (str(i) + " flaskor läsk i kylen, " + str(i) + " flaskor läsk.")
+        print ("Ta ner en och skicka den vidare, " + str(i) + " flaskor läsk i kylen.")
+    elif i == 1:
+        print (str(i) + " flaska läsk i kylen, " + str(i) + " flaska läsk.\n Ta ner en och skicka den vidare, inga flaskor läsk i kylen.")
+    else:
+        print ("Inga fler flaskor läsk i kylen, inga fler flaskor läsk. \n Gå till affären och köp lite mer, 99 flaskor läsk i kylen")
+    
+    
+        
+        
+    
